ef656992504fab6424cec80cf4926feea7d6a512
PFunology0612 <voongtieuphuong@gmail.com>
2023-12-14 12:20:02 +0700

    first commit

---
+++ bt_pointer.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct {
+	int *p;
+	int size;
+} array;
+
+array ptr;
+
+int main(){
+	
+	printf("Nhap so luong phan tu: ");
+	scanf("%d", &ptr.size);
+	
+	// Cap phat dong cho con tro cap 1 (ptr.p)
+	ptr.p = (int*)malloc(ptr.size * sizeof(int));
+	
+	// Cap phat dong cho con tro cap 2
+	int **p_to_p = &ptr.p;
+	
+	// Nhap phan tu cho mang con tro cap 1 
+	for(int i = 0; i < ptr.size; i++){
+		scanf("%d", &ptr.p[i]);
+	}
+	
+	printf("Cac gia tri cua con tro cap 1: ");
+	for(int i = 0; i < ptr.size; i++){
+		printf("%d ", *(ptr.p + i));
+	}
+	printf("\n");
+	for(int i = 0; i < ptr.size; i++){
+		printf("%d ", ptr.p[i]);
+	}
+	return 0;
+}
